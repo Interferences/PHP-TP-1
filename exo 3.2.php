ff0a12cf659f5d39383275c97378e610357f6045
philippequentin91 <philippequentin91@gmail.com>
2016-04-06 06:29:13 -0400

    exo bookmark

---
+++ exo 3.2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>hello world en php !</title>
+</head>
+<body>
+
+<?php
+  $repertoire=array(
+    "Moteur"=>array(
+      "Google"=>array("url=https://www.google.fr/?gws_rd=ssl","img"),
+      "Yahoo"=>array("url=https://fr.yahoo.com/","img")
+      ),
+    "Journaux"=>array(
+      "Le Monde"=>array("url=http://www.lemonde.fr/","img"),
+      "Linux"=>array("url=https://boutique.ed-diamond.com/7-gnulinux-magazine", "img="),
+      "L'Equipe"=>("url=http://www.lequipe.fr/", "img=")
+    ),
+    "Sport"=>array(
+      "FFF"=>array("url=http://www.fff.fr/","img="),
+      ""
+    )
+  )
+  
+?>
+
+</body>
+</html>
